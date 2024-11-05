60ab6686655052b3b1ead59490a52d2216f2d149
Sally-009 <skojima2025@gmail.com>
2024-11-05 11:23:47 -0600

    Created Components for welcome page

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,6 +1,8 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import React from "react";
 
 export default function Index() {
+
   return (
     <View
       style={{
@@ -9,7 +11,6 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
