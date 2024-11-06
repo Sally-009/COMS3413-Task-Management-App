a4670a44116e589d7824fff4486a644e77389718
Sally-009 <skojima2025@gmail.com>
2024-11-05 18:57:22 -0600

    Finished Welcome Page and set up navigation (need modification for better performance)

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,9 +1,33 @@
-import { Stack } from "expo-router";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+// Import pages
+import WelcomePage from "./welcome-page";
+import LoginPage from "./login-page";
+import RegisterPage from "./register-page";
+
+// Create navigation stack
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="welcome-page"
+        component={WelcomePage}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="login-page"
+        component={LoginPage}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="register-page"
+        component={RegisterPage}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
   );
 }
---
+++ frontend/app/register-page.js
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text, Image } from "react-native";
+import React from "react";
+
+export default function RegisterPage() {
+    return (
+        <SafeAreaView>
+        <Text style={{ textAlign: "center", fontSize: 20 }}>
+            Register for the app!
+        </Text>
+        </SafeAreaView>
+    );
+}
