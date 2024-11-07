a11cbad26fe8f3dce5f37aa1d53ecb1337e708a0
Sally <skojima2025@gmail.com>
2024-11-06 20:13:06 -0600

    Cleaned up index.tsx

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,23 +1,7 @@
 import { Text, View, StyleSheet, TextInput } from "react-native";
 import React, { useState } from "react";
 
-import InputFieldTask from "../components/input-field-task";
-import TaskItem from "../components/task-item";
-
 export default function Index() {
-  // State management
-  const [isCompleted, setIsCompleted] = useState(false);
-  const [taskName, setTaskName] = useState("");
-
-  // Function to handle task name changes
-  const onChangeText = (text: string) => {
-    setTaskName(text);
-  };
-
-  // Function to handle checkbox state changes
-  const onCheckBoxChange = (value: boolean) => {
-    setIsCompleted(value);
-  };
 
   return (
     <View
@@ -28,37 +12,7 @@ export default function Index() {
         backgroundColor: "#fff",
       }}
     >
-      <InputFieldTask
-        iconName={"edit"}
-        value={taskName}
-        onChangeText={onChangeText}
-        placeholder={"Enter task name"}
-      />
 
-      <InputFieldTask
-        iconName={"label"}
-        value={taskName}
-        onChangeText={onChangeText}
-        placeholder={"Enter task category"}
-      />
-
-      <InputFieldTask
-        iconName={"subject"}
-        value={taskName}
-        onChangeText={onChangeText}
-        placeholder={"Enter task description"}
-        height={100}
-        multiline={true}
-        alignItems="flex-start"
-      />
-
-      <TaskItem
-        isCompleted={isCompleted}
-        taskName="Task 1"
-        description={"This is a test!"}
-        categoryColor="blue"
-        onCheckBoxChange={onCheckBoxChange}
-      />
     </View>
   );
 }
