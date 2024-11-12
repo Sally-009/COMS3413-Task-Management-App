9264a8005747436d315bf565fca4b48cf2e07f9b
Sally <skojima2025@gmail.com>
2024-11-11 22:08:11 -0600

    applied common styles

--- frontend/app/tab/setting-page.js
+++ frontend/app/tab/setting-page.js
@@ -1,9 +1,10 @@
 import { SafeAreaView, View, Text } from "react-native";
 import React from "react";
+import { styles } from "../styles";
 
 export default function SettingPage() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <View>
         <Text>Setting Page</Text>
       </View>
